a31139ae9c266cba1bd2306bed0ce9862dd37238
Gong Zhang <zg25634153@gmail.com>
2017-08-03 06:03:17 +0000

    added tweak function for model Post

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,8 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
+from django.utils import timezone
+import datetime
 # Create your models here.
 
 class Post(models.Model):
@@ -12,3 +13,6 @@ class Post(models.Model):
     
     def __str__(self):
         return self.title
+        
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
