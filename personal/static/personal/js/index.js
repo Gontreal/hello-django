5d578f9e5a86c81b82e8891e454ca8046e8b8e94
Gong Zhang <zg25634153@gmail.com>
2017-08-17 20:55:49 -0400

    Minor update on scrollspy

--- personal/static/personal/js/index.js
+++ personal/static/personal/js/index.js
@@ -3,7 +3,7 @@
 //$('body').scrollspy({ target: '#sidebar' })
 $( document ).ready(function() {
     
-    $('body').scrollspy({ target: '#sidebar',offset:80 });
+    $('body').scrollspy({ target: '#sidebar',offset:200 });
     //$("#tar2").addClass("active");
    // $('#sidebar-wrapper').css('background-color','black');
     console.log( "ready!" );
